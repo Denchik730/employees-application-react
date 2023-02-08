83372d5436c00c93e1f86ebcda638332d983c79d
Denis Gabbasov <gabbas.denn@mail.ru>
2023-02-08 05:57:35 +0300

    feat:реализовать поиск сотрудника через поисковую строку

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -16,7 +16,8 @@ class App extends Component {
         {name: 'Denis G.', salary: 800, increase: false,rise: true, id: 1},
         {name: 'Alex B.', salary: 3000, increase: true,rise: false, id: 2},
         {name: 'Oleg G.', salary: 15000, increase: false,rise: false, id: 3},
-      ]
+      ],
+      term: ''
     }
     this.maxId = 4;
   }
@@ -60,20 +61,37 @@ class App extends Component {
     }));
   }
 
+  searchEmp = (items, term) => {
+    if (term.length === 0) {
+      return items;
+    }
+
+    return items.filter(item => {
+      return item.name.indexOf(term) > -1;
+    });
+  }
+
+  onUpdateSearch = (term) => {
+    this.setState({term: term});
+  }
+
   render() {
+    const {data, term} = this.state;
     const employees = this.state.data.length;
     const increasedEmployees = this.state.data.filter(item => item.increase).length;
+    const visibleData = this.searchEmp(data, term);
+
     return (
       <div className="app">
         <AppInfo employees={employees} increasedEmployees={increasedEmployees}/>
   
         <div className="search-panel">
-          <SearchPanel/>
+          <SearchPanel onUpdateSearch={this.onUpdateSearch}/>
           <AppFilter/>
         </div>
   
         <EmployeesList 
-          data={this.state.data}
+          data={visibleData}
           onDelete={this.deleteItem}
           onToggleProp={this.onToggleProp}/>
         <EmployeesAddForm
